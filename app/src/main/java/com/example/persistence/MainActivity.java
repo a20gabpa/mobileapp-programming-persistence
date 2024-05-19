e5cbde04e8ef2a161b130a7f04d503607af5801f
Gabriel Gomez Palomäki <a20gabpa@student.his.se>
2024-05-19 21:09:15 +0200

    Implemented read/write functionality
    Added the possibility to add and read from the database. This is done by separate buttons.

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -2,17 +2,25 @@ package com.example.persistence;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     /* =============== VARIABLES =============== */
     private Button writeBtn, readBtn;
     private EditText editName, editType, editAcro;
+    private TextView dbText;
     private SQLiteDatabase db;
     private DatabaseHelper dbHelper;
     /* ========================================= */
@@ -29,5 +37,54 @@ public class MainActivity extends AppCompatActivity {
         editName = findViewById(R.id.topEditText);
         editType = findViewById(R.id.middleEditText);
         editAcro = findViewById(R.id.lowerEditText);
+        dbText = findViewById(R.id.dbText);
+
+        // Initialize
+        dbHelper = new DatabaseHelper(this);
+        db = dbHelper.getWritableDatabase();
+
+        // Write to database when clicked
+        writeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ContentValues myValues = new ContentValues();
+                myValues.put(DatabaseTables.Country.COLUMN_NAME_ID, editName.getText().toString());
+                myValues.put(DatabaseTables.Country.COLUMN_NAME_TYPE, editType.getText().toString());
+                myValues.put(DatabaseTables.Country.COLUMN_NAME_ACRONYM, editAcro.getText().toString());
+
+                // Insert to database
+                db.insert(DatabaseTables.Country.TABLE_NAME, null, myValues);
+
+                // Empty fields
+                editName.setText("");
+                editType.setText("");
+                editAcro.setText("");
+            }
+        });
+
+        // Read from database when clicked
+        readBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cursor cursor = db.query(DatabaseTables.Country.TABLE_NAME, null,
+                        null, null, null, null, null);
+
+                List<String> countries = new ArrayList<>();
+                while (cursor.moveToNext()) {
+                    int countryID = cursor.getInt(cursor.getColumnIndexOrThrow(DatabaseTables.Country.COLUMN_NAME_ID));
+                    String countryName = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseTables.Country.COLUMN_NAME_TYPE));
+                    String countryAcro = cursor.getString(cursor.getColumnIndexOrThrow(DatabaseTables.Country.COLUMN_NAME_ACRONYM));
+
+                    countries.add("ID: "+ countryID + " Name: " + countryName + " Acronym: " + countryAcro);
+                }
+                cursor.close();
+
+                String fancyText = "";
+                for (String country : countries) {
+                    fancyText = fancyText + country + "\n";
+                }
+                dbText.setText(fancyText);
+            }
+        });
     }
 }
