20ee0d367a6f3ac2ed0ab95dd92e5e9316ad4623
Gabriel Gomez Palomäki <a20gabpa@student.his.se>
2024-05-19 19:55:05 +0200

    Get references from view and name refactor
    Added a way of getting the elements from the view and access them in main activity. Also updated the name for one of the fields.

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.persistence;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    /* =============== VARIABLES =============== */
+    private Button writeBtn, readBtn;
+    private EditText editName, editType, editAcro;
+    /* ========================================= */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get references to buttons and text-fields
+        writeBtn = findViewById(R.id.writeBtn);
+        readBtn = findViewById(R.id.readBtn);
+
+        editName = findViewById(R.id.topEditText);
+        editType = findViewById(R.id.middleEditText);
+        editAcro = findViewById(R.id.lowerEditText);
     }
 }
